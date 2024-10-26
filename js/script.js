a61911fac0c9a39616dfaf962db7a70648f2c7a0
Jejekdf <maulanarandi531@gmail.com>
2024-10-26 23:41:48 +0700

    good

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
-var pages = new Array('halaman1', 'halaman2', 'halaman3', 'halaman4');
-
-function klikMenu() {
-  var menuButton = document.getElementsByClassName('tombol_menu')[0];
-  var kontener = document.getElementsByClassName('dalemnya_kontener')[0];
-
-  // Toggle the class to show or hide the cross icon
-  menuButton.classList.toggle('open');
-  kontener.classList.toggle('buka_menu');
-}
-
 function PindahKeHalaman(page) {
   var dalemnya_kontener = document.getElementsByClassName('dalemnya_kontener')[0];
   var sections = document.getElementsByClassName('dalemnya_halaman');
+  
   for (i = 0; i < sections.length; i++) {
-    sections[i].classList.remove('before', 'after');
+    sections[i].classList.remove('before', 'after', 'active'); // Hapus kelas aktif
     if (i > page) {
       sections[i].classList.add('after');
+    } else {
+      sections[i].classList.add('active'); // Tambahkan kelas aktif
     }
   }
+  
   dalemnya_kontener.classList.remove('buka_menu', 'page-halaman1', 'page-halaman2');
   dalemnya_kontener.classList.add('page-' + pages[page]);
+}
+
+function closePage(page) {
+  var sections = document.getElementsByClassName('dalemnya_halaman');
+  
+  // Hapus kelas aktif dari section yang diklik
+  sections[page].classList.remove('active');
+
+  // Menampilkan tombol menu lagi jika semua section ditutup
+  var allSectionsClosed = true;
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].classList.contains('active')) {
+      allSectionsClosed = false;
+      break;
+    }
+  }
+
+  if (allSectionsClosed) {
+    var menuButton = document.getElementsByClassName('tombol_menu')[0];
+    menuButton.classList.remove('open');
+    var kontener = document.getElementsByClassName('dalemnya_kontener')[0];
+    kontener.classList.remove('buka_menu');
+  }
 }
